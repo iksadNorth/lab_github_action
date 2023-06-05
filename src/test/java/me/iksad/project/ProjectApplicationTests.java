3f053743ea3fcee536e91699d92c5b328aa36a6a
박정훈 <66674140+iksadNorth@users.noreply.github.com>
2023-06-05 18:29:24 +0900

    Initial commit

---
+++ src/test/java/me/iksad/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iksad.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
